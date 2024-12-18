053a5c646b93a75a1ea10019490f70042f8fcddf
yousef tbakhi <yussuftbakhi@gmail.com>
2024-12-18 11:01:02 +0300

    first one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
